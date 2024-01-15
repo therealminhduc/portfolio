d5bde27aecb3c5877d8737ef190a17d0c3ed347c
minhduc <ducbv1407@gmail.com>
2024-01-15 21:40:08 +0100

    [WIP] bento grid

---
+++ portfolio/src/components/bentoGrid/data.ts
@@ -0,0 +1,23 @@
+export const data = [
+    {
+        title: 'Title 1'
+    },
+    {
+        title: 'Title 2'
+    },
+    {
+        title: 'Title 3'
+    },
+    {
+        title: 'Title 4'
+    },
+    {
+        title: 'Title 5'
+    },
+    {
+        title: 'Title 6'
+    },
+    {
+        title: 'Title 7'
+    },
+];
--- portfolio/src/main.ts
+++ portfolio/src/main.ts
@@ -1,6 +1,7 @@
 import {HeaderComponent} from "./components/header/HeaderComponent.ts";
 import {FooterComponent} from "./components/footer/FooterComponent.ts";
 import {CursorComponent} from "./common/cursor/CursorComponent.ts";
+import {BentoGridComponent} from "./components/bentoGrid/BentoGridComponent.ts";
 
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Portfolio loaded!');
@@ -14,6 +15,12 @@ document.addEventListener('DOMContentLoaded', () => {
 document.addEventListener('DOMContentLoaded', () => {
     const headerComponent = new HeaderComponent();
     headerComponent.render(document.querySelector<HTMLDivElement>('#app')!);
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const bentoGridComponent = new BentoGridComponent();
+    bentoGridComponent.addItem();
+    bentoGridComponent.render(document.querySelector<HTMLDivElement>('#app')!);
 });
 
 document.addEventListener('DOMContentLoaded', () => {
